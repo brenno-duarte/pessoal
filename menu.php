bb70f00700f4cc9baa1881cb37f9b88f2fa7f0f8
brennoDuarte <brenno.gnr@gmail.com>
2019-06-29 18:19:09 -0300

    nova logo

--- menu.php
+++ menu.php
@@ -2,22 +2,26 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
 	<title>Brenno Duarte - Programador front-end e back-end</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="css/custom.css">
-	<link type="text/css" rel="icon" href="img/logo/logo_mini.png">
+	<link type="text/css" rel="icon" href="img/logo/logopessoalmini.png">
 </head>
-
 <body>
 	<div class="navbar-fixed">
 		<nav class="z-depth-0 light-blue accent-4">
 			<div class="nav-wrapper container">
 				<a href="index.php" class="brand-logo nome">
-					<img src="img/logo/LOGO3.png" class="responsive-img">
+					<img src="img/logo/logopessoalbranca.png" class="responsive-img">
 				</a>
 				<a href="#" data-target="mobile-demo" class="sidenav-trigger">
-					<span>&#9776;</span>
+					<i class="material-icons">menu</i>
 				</a>
 				<ul class="right hide-on-med-and-down">
 					<li><a href="projetos.php">Projetos pessoais</a></li>
@@ -29,6 +33,9 @@
 	</div>
 
 	<ul class="sidenav" id="mobile-demo">
+		<div style="margin: 10px 0px 0px 10px">	
+			<li><img src="img/logo/logopessoalmenu.png" alt=""></li>
+		</div>
 		<li><a href="projetos.php">Projetos pessoais</a></li>
 		<li><a href="servicos.php">Serviços</a></li>
 	</ul>
