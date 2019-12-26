28d736aaf2e183da36e82c7f8ba9a42aeb652c7c
brennoDuarte <brenno.gnr@gmail.com>
2019-12-26 16:12:02 -0300

    removendo loja para outro repositório

--- Store/Sources/Model/Model.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Model {
-    private $nome;
-}
--- Store/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'Config.php';
-require 'dependences.php';
-require 'Router/rIndex.php';
-require 'Router/rDev.php';
-require 'Router/rBusca.php';
-
-$app->run();
